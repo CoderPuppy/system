daca29fd2bde1f851bae9706f005a72385923c5c
Drew Young <drew@drew-ubuntu-1204-vbox>
2012-09-26 12:46:32 -0400

    Fix loading ruby.sh

--- sh/continue.sh
+++ sh/continue.sh
@@ -4,7 +4,30 @@
 #   - Git
 #   - Package Manager Support
 
+resolve_link() {
+  $(type -p greadlink readlink | head -1) "$1"
+}
+
+abs_dirname() {
+  set -x
+  local cwd path
+  cwd="`pwd`"
+  path="$1"
+
+  while [ -n "$path" ]; do
+    cd "${path%/*}"
+    local name="${path##*/}"
+    path="$(resolve_link "$name" || true)"
+  done
+
+  pwd
+  cd "$cwd"
+}
+
+base="`abs_dirname $0`"
+
+
 # Get Ruby
-. ./ruby.sh
+. $base/ruby.sh
 
 ruby ../ruby/continue.rb
