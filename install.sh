84ac51f2758f704da453ae7e242285ad737af2d5
Drew Young <drewyoung1@gmail.com>
2012-09-26 08:40:05 -0400

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+super() {
+	if which sudo; then
+		sudo $*
+	else
+		echo "No known way to become root"
+		exit 1
+	fi
+}
+
+pkg_mng_apt() {
+	command=$1
+
+	shift
+
+	case $command
+	install)
+		super apt-get install $*
+		;;
+	esac
+}
+
+pkg_mng_yum() {
+	command=$1
+	
+	shift
+		
+	case command in
+	install)
+		super yum install $@
+		;;
+	esac
+}
+
+pkg_mng=""
+
+pkg_mng() {
+	if [ "$pkg_mng" = "" ]; then
+		pkg_mng=`find_pkg_mng`
+	fi
+	
+	echo "using $pkg_mng"
+	
+	eval "pkg_mng_$pkg_mng $*"
+}
+
+find_pkg_mng() {
+	if which aptitude &> /dev/null; then
+		echo "apt"
+	elif which yum &> /dev/null; then
+		echo "yum"
+	else
+		echo "No known package manager"
+		exit 1
+	fi
+}
+
+pkg_mng install git
+
+git clone git://github.com/system-setup/system /tmp/system
